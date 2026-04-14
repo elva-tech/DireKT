a5328b9ae090b159da6511e45ba2fc14e04aa6ff
Sindhu <sindhuksatish3@gmail.com>
2026-04-14 19:57:18 +0530

    Deployment Final Fix: Switch to HashRouter for bulletproof SPA navigation on Render

--- ssss/frontend/src/main.jsx
+++ ssss/frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <Toaster
           position="top-right"
@@ -44,7 +44,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             },
           }}
         />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>
 )
